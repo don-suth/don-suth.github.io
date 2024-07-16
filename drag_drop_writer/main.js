02de924b7462ecbdba585666deba2170e920a9d5
Donald Sutherland <donald@sutherland.id.au>
2024-07-16 12:57:37 +0800

    Drag and Drop working really well

--- drag_drop_writer/main.js
+++ drag_drop_writer/main.js
@@ -1,5 +1,18 @@
+let canvas;
+
 function dragElement(event) {
-    document.getElementById("feedback").innerText = event.target.innerText;
+    event.dataTransfer.setData("char", event.target.innerText);
+    document.getElementById("feedback").innerText = event.dataTransfer.getData("char");
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function dropElement(event) {
+    event.preventDefault();
+    document.getElementById("feedback").innerText = "Dropped " + event.dataTransfer.getData("char");
+
 }
 
 function createLetter(letter) {
@@ -15,6 +28,7 @@ function initialise() {
     let alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
     alphabet.forEach(createLetter);
     document.getElementById("feedback").innerText = "";
+    canvas = new fabric.Canvas(document.getElementById("canvas"));
 }
 
 document.addEventListener("DOMContentLoaded", initialise);

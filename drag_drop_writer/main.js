90d3d8b4b3a895acf3077a3e7aefd1ff4cb9f32b
Donald Sutherland <donald@sutherland.id.au>
2024-07-16 12:11:49 +0800

    Touching on letters works

--- drag_drop_writer/main.js
+++ drag_drop_writer/main.js
@@ -1,3 +1,17 @@
 function dragElement(event) {
-    document.getElementById("feedback").innerText = "Did the thing!";
+    document.getElementById("feedback").innerText = event.target.innerText;
+}
+
+function createLetter(letter) {
+    let mySpan = document.createElement("span")
+    mySpan.draggable = true;
+    mySpan.className = "draggable_text";
+    mySpan.innerText = letter;
+    mySpan.addEventListener("dragstart", dragElement);
+    document.getElementById("text_container").appendChild(mySpan);
+}
+
+window.onload = function() {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    alphabet.forEach(createLetter);
 }
